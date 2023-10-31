7209c678c25cc93ed3621cbbf5ab4dd756dd78c8
Vathsalyatha <147684402+Vathsalyatha@users.noreply.github.com>
2023-10-31 11:23:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let Celsius = document.getElementById("celsius");
+let Fahrenheit = document.getElementById("fahrenheit");
+let Kelvin = document.getElementById("kelvin");
+
+Celsius.oninput = function () {
+    let f = (parseFloat(Celsius.value)*9)/5+32;
+    Fahrenheit.value = parseFloat(f.toFixed(2));
+    let k = parseFloat(Celsius.value)+273.15;
+    Kelvin.value = parseFloat(k.toFixed(2));
+}
+Fahrenheit.oninput = function () {
+    let c = ((parseFloat(Fahrenheit.value)-32)*5)/9;
+    Celsius.value = parseFloat(c.toFixed(2));
+    let k = ((parseFloat(Fahrenheit.value)-32)*5)/9 + 273.15;
+    Kelvin.value = parseFloat(k.toFixed(2));
+}
+Kelvin.oninput = function () {
+    let f = ((parseFloat(Kelvin.value)-273.15)*9)/5+32;
+    Fahrenheit.value = parseFloat(f.toFixed(2));
+    let c = parseFloat(Kelvin.value)-273.15;
+    Celsius.value = parseFloat(c.toFixed(2));
+}
